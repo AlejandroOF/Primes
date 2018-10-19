696e879a02f628ceaff821f0f9b25304939a817a
Alejandro Ortiz <alejandro.ortiz@unosquare.com>
2018-10-19 13:12:13 -0500

    first commit

--- primos/Program.cs
+++ primos/Program.cs
@@ -6,7 +6,41 @@ namespace primos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Ingrese un numero");
+            int a = Int32.Parse(Console.ReadLine());
+            bool primo = true;
+
+            if (a >= 1000)
+            {
+                Console.WriteLine("Error");
+
+            }
+
+            for (int i = 2; i <= a; i++)
+            {
+                for (int j = 2; j <= a; j++)
+                {
+
+                    if (i != j && i % j == 0)
+                    {
+                        primo = false;
+                        break;
+
+                    }
+
+                }
+                if (primo)
+                {
+                    Console.WriteLine(i);
+                }
+
+                primo = true;
+            }
+            Console.ReadKey();
         }
     }
+
 }
+
+
